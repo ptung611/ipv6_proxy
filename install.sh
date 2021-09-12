b6e9991dc9ce75551969999ff1d35ff5aa71cbfc
ptung611 <phitung0611@gmail.com>
2021-09-12 22:44:23 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ gen64() {
 }
 install_3proxy() {
   echo "installing 3proxy"
-  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  #URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  URL="https://github.com/z3APA3A/3proxy-archive/blob/master/0.8.6/3proxy-0.8.6.tgz"
   wget -qO- $URL | bsdtar -xvf-
   cd 3proxy-3proxy-0.8.6
   make -f Makefile.Linux
